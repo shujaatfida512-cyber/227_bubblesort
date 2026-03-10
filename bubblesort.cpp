d1b76c11810c1e4c9334b1ad38adb4cd501f3f26
shujaatfida512-cyber <shujaatfida512@gmail.com>
2026-03-10 15:39:42 +0700

    Translate comments and prompts to English
    Updated comments and prompts in the Bubble Sort algorithm to English.

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -4,46 +4,46 @@
 #include <string>
 using namespace std;
 
-int arr[20]; // Deklarasi variable global array a dengan ukuran 20
-int n;    // Deklarasi variabel global n untuk menyimpan banyaknya elemen pada array
+int arr[20]; // Declaration of global array arr with size 20
+int n;       // Declaration of global variable n to store the number of elements in the array
 
 void input()
 {
-    // procedur untuk input
+    // procedure for input
     int d;
     while (true)
     {
-        cout << "Masukan banyaknya elemen pada array : "; // Output ke layar
-        cin >> n;    // Input dari pengguna
-        if (n <= 20)    // Jika n kurang dari atau sama dengan 20
+        cout << "Enter the number of elements in the array: "; // Output to screen
+        cin >> n;    // Input from user
+        if (n <= 20)    // If n is less than or equal to 20
             break;
         else
         {
-            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"; // Jika n lebih dari 20
+            cout << "\nArray can have a maximum of 20 elements.\n"; // If n is more than 20
         }
     }
 
     cout << endl;
     cout << "======================" << endl;
-    cout << "Masukan Elemen Array" << endl;
+    cout << "Enter Array Elements" << endl;
     cout << "======================" << endl;
 
     for (int i = 0; i < n; i++)
     {
-        cout << "Data ke-" << (i + 1) << ": "; // Looping dengan i dimulai dari 0 hingga n-1
-        cin >> arr[i];    // Input dari pengguna
+        cout << "Data-" << (i + 1) << ": "; // Looping with i starting from 0 to n-1
+        cin >> arr[i];    // Input from user
     }
 }
 
 void bubbleSort()
 {
-    // Procedure untuk bubble sort
-    int pass = 1; // step untuk melintasi array dimulai dari 1
+    // Procedure for bubble sort
+    int pass = 1; // step for traversing the array starting from 1
     do
     {
         for (int j = 0; j < n - pass; j++)
         {
-            if (arr[j] > arr[j + 1]) // jika data ke j lebih besar dari data ke j+1, lakukan swap
+            if (arr[j] > arr[j + 1]) // if data at index j is greater than data at index j+1, perform swap
             {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
@@ -53,20 +53,20 @@ void bubbleSort()
         pass++;
     } while (pass <= n - 1);
     
-    cout << "\nArray telah diurutkan dengan Bubble Sort!" << endl;
+    cout << "\nArray has been sorted using Bubble Sort!" << endl;
 }
 
 void display()
 {
-    // Procedure untuk menampilkan hasil
+    // Procedure to display results
     cout << endl;
     cout << "=================================" << endl;
-    cout << "Element Array yang telah tersusun" << endl;
+    cout << "Sorted Array Elements" << endl;
     cout << "=================================" << endl;
 
     for (int i = 0; i < n; i++)
     {
-        cout << arr[i] << " "; // menampilkan data array
+        cout << arr[i] << " "; // displaying array data
     }
     cout << endl;
 }
